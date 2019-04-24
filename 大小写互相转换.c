346281c09a36a1c1b4e353433d2a507dc4d85cf7
kobe1916 <47083794+kobe1916@users.noreply.github.com>
2019-04-24 20:21:34 +0800

    Create 大小写互相转换.c

---
+++ 大小写互相转换.c
@@ -0,0 +1,21 @@
+int main()
+{
+	int ch;
+	printf("请输入一个字符:\n");
+	while ((ch = getchar()) != EOF)
+	{
+		if (ch >= 'a'&&ch <= 'z')
+			printf("%c\n", ch - 32);
+		else
+			if (ch >= 'A'&&ch <= 'Z')
+				printf("%c\n", ch + 32);
+			else
+				if (ch >= '0'&&ch <= '9')
+					;
+				else
+					;
+	}
+	printf("\n");
+	system("pause");
+	return 0;
+}
