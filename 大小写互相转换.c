f92a089134afb4353324a2c9c1b5beb2fcd04f8b
kobe1916 <47083794+kobe1916@users.noreply.github.com>
2019-04-24 20:37:37 +0800

    Update 大小写互相转换.c

--- 大小写互相转换.c
+++ 大小写互相转换.c
@@ -1,17 +1,17 @@
 int main()
 {
-	int ch;
+	int ch;			//根据ASCII码，大小写字母对应的数值  所以用int
 	printf("请输入一个字符:\n");
-	while ((ch = getchar()) != EOF)
+	while ((ch = getchar()) != EOF)		//EOF--表示文件结束符（end of file）
 	{
-		if (ch >= 'a'&&ch <= 'z')
+		if (ch >= 'a'&&ch <= 'z')		//小写转为大写
 			printf("%c\n", ch - 32);
 		else
-			if (ch >= 'A'&&ch <= 'Z')
+			if (ch >= 'A'&&ch <= 'Z')	//大写转为小写
 				printf("%c\n", ch + 32);
 			else
-				if (ch >= '0'&&ch <= '9')
-					;
+				if (ch >= '0'&&ch <= '9')	//判断是否为数字
+					;		//  ；空语句表述不输出
 				else
 					;
 	}
