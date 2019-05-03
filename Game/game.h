7739b2cdab665209a21bc9ec6c8ab5f1e8306f7e
kobe1916 <47083794+kobe1916@users.noreply.github.com>
2019-05-03 22:48:43 +0800

    Update game.h

--- Game/game.h
+++ Game/game.h
@@ -1,4 +1,4 @@
-#define _CRT_SECURE_NO_WARNINGS 1
+  #define _CRT_SECURE_NO_WARNINGS 1
 
 
 #include <stdio.h>
@@ -12,5 +12,6 @@
 //函数的定义   //分号！！！
 void ShowBoard(char a[Row][Col], int row, int col);		//打印棋盘
 void Board(char a[Row][Col],int row, int col);		//棋盘的初始化(全部为' '）
-void Player(char a[Row][Col], int row, int col);
-void Compuer(char a[Row][Col], int row, int col);
+void Player(char a[Row][Col], int row, int col);  //玩家走
+void Compuer(char a[Row][Col], int row, int col);   //电脑走
+                                                     //判断输赢
