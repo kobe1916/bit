214c0a86501ac31dd51ac9ff61c978be3131cd7f
kobe1916 <47083794+kobe1916@users.noreply.github.com>
2019-05-07 23:14:25 +0800

    Update test.c

--- Game/test.c
+++ Game/test.c
@@ -31,6 +31,45 @@ void menu()//只进行打印菜单功能
 //	}
 
 
+void game()
+{
+	//进入游戏后先打印网格图（定义二维数组）
+	char a[Row][Col] = { 0 };
+	char ret = 0;
+	//数组初始化  打印网格图
+	Board(a,Row,Col);
+	ShowBoard(a,Row,Col);
+
+	//进入游戏,未分出胜负之前一直进行游戏
+	while (1)
+	{
+		Player(a, Row, Col);
+		ShowBoard(a, Row, Col);
+		//int ret = IsWin(a, Row, Col);
+		//' '--继续走
+		//*--玩家赢
+		//#--电脑赢
+		//0--平局
+		//
+		ret = IsWin(a, Row, Col);
+		//if (ret == ' ')
+		if (ret != ' ')
+			break;
+		Computer(a, Row, Col);
+		ShowBoard(a, Row, Col);//下完棋后展示棋盘
+		ret =IsWin(a, Row, Col);
+		if (ret != ' ')
+			break;
+	}
+	if (ret == '*')
+		printf("恭喜你，获胜了！\n");
+	else if (ret == '#')
+		printf("很遗憾，你输了。。。\n");
+	else
+		printf("平局\n");
+
+
+
 
 int main()
 {
