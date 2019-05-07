74b01040db84032e8272e33d97a950662efb7771
kobe1916 <47083794+kobe1916@users.noreply.github.com>
2019-05-07 23:12:02 +0800

    Update game.h

--- Game/game.h
+++ Game/game.h
@@ -14,4 +14,4 @@ void ShowBoard(char a[Row][Col], int row, int col);		//打印棋盘
 void Board(char a[Row][Col],int row, int col);		//棋盘的初始化(全部为' '）
 void Player(char a[Row][Col], int row, int col);  //玩家走
 void Compuer(char a[Row][Col], int row, int col);   //电脑走
-                                                     //判断输赢
+                                                   
