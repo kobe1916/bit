6bffed476791f46910467d5a0f99c93bd05b25e0
kobe1916 <47083794+kobe1916@users.noreply.github.com>
2019-05-09 23:57:21 +0800

    Update test.c

--- Games/test.c
+++ Games/test.c
@@ -1,1 +1,41 @@
 
+#define _CRT_SECURE_NO_WARNINGS 1
+#include "game.h"
+
+void menu()
+{
+	printf("********************\n");
+	printf("***    1.PLAY    ***\n");
+	printf("***    0.EXIT    ***\n");
+	printf("********************\n");
+	
+}
+
+int main()
+{
+	int n = 0;
+
+	char mine[Row][Col];
+	char show_mine[Rows][Cols];
+	do
+	{
+		printf("请输入你的选择：\n");
+		menu();
+		scanf("%d", &n);
+		switch (n)
+		{
+		case 1:game(); break;
+		case 0:printf("退出游戏！\n"); break;
+		default:
+			printf("输入错误，请重新输入。\n"); break;
+		}
+	} while (n);
+
+
+
+
+
+	
+	system("pause");
+	return 0;
+}
