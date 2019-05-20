3c9723d5dbe4951af09ce98e46f7edc0a927dc1c
kobe1916 <47083794+kobe1916@users.noreply.github.com>
2019-05-20 19:24:20 +0800

    Update 杨辉三角.c

--- 杨辉三角.c
+++ 杨辉三角.c
@@ -1,1 +1,38 @@
+//杨辉三角
+#include <stdio.h>
+#include <stdlib.h>
 
+#define N 10
+
+int main()
+{
+	int i;
+	int j;
+	int a[N][N];
+	for (i = 0; i < N; i++)
+	{
+		a[i][0] = 1;
+		a[i][i] = 1;
+	}
+	for (i = 2; i < N; i++)
+	{
+		for (j = 1; j < i; j++)
+		{
+			a[i][j] = a[i - 1][j - 1] + a[i - 1][j];
+		}
+	}
+	for (i = 0; i < N; i++)
+	{
+		for (j = 0; j < (2 * N - 2 * i); j++)
+		{
+			printf(" ");
+		}
+		for (j = 0; j <= i; j++)
+		{
+			printf("%4d", a[i][j]);
+		}
+		printf("\n");
+	}
+	system("pause");
+	return 0;
+}
