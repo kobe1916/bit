c499f8d12ab8d21b00912bb29272346889d1d2f5
kobe1916 <47083794+kobe1916@users.noreply.github.com>
2019-05-23 23:07:12 +0800

    Update 判断凶手.c

--- 判断凶手.c
+++ 判断凶手.c
@@ -8,3 +8,18 @@ D说：C在胡说
 已知3个人说了真话，1个人说的是假话。
 现在请根据这些信息，写一个程序来确定到底谁是凶手。
 */
+int main()
+{
+	int murder;
+	for (murder = 'A'; murder <= 'D'; murder++)      //ASC码
+	{
+		if (((murder != 'A') + (murder == 'C') + (murder == 'D') + (murder != 'D')) == 3)    //三真一假，在if语句的，括号里为真结果为1，要是假结果为0。因此if括号  后面若成立，必定为三真一假，即必定等于3.  
+		{
+			printf("murder is %c\n", murder);     //由于最终要输出凶手，为一个字符，%c可用输出字符  
+
+		}
+
+	}
+	system("pause");
+	return 0;
+}
