24f7a993bc4623ace3155b3b7e59c0f582021864
kobe1916 <47083794+kobe1916@users.noreply.github.com>
2019-05-28 16:30:52 +0800

    Update 调整数组使奇数全部都位于偶数前面.c

--- 调整数组使奇数全部都位于偶数前面.c
+++ 调整数组使奇数全部都位于偶数前面.c
@@ -35,3 +35,69 @@ void separate(int arr[], int left, int right)
 		}
 	}
 }
+
+
+int main()
+{
+	int arr[] = { 1,3,2,4,6,7,8,9,10,11 };
+	int left = 0;
+	int right = sizeof(arr) / sizeof(arr[0]) - 1;
+
+	separate(arr, left, right);
+	Print(arr, right);
+	system("pause");
+	return 0;
+}
+
+
+
+
+//调整数组使奇数全部都位于偶数前面
+void Depart_Arry(int arr[], int size)
+{
+	int left = 0;
+	int right = size - 1;
+	while (left < right)
+	{
+		//从前到后找偶数
+		while (left < right&&arr[left] % 2 != 0)
+		{
+			left++;
+		}
+		//从后到前找奇数
+		while (left < right&&arr[right] % 2 == 0)
+		{
+			right--;
+		}
+		//交换
+		if (left < right)
+		{
+			int tmp = arr[left];
+			arr[left] = arr[right];
+			arr[right] = tmp;
+		}
+	}
+}
+
+//打印数组
+void Print(int arr[], int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+}
+
+int main()
+{
+
+	int arr[] = { 2, 3, 4, 6, 5, 8, 7, 10, 9, 1 };
+	int size = sizeof(arr) / sizeof(arr[0]);
+	Depart_Arry(arr, size);
+	Print(arr, size);
+	system("pause");
+	return 0;
+}
+
+
