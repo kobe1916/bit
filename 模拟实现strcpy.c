3de19409d2bd630060d2cb76afb2b18f6e60310f
kobe1916 <47083794+kobe1916@users.noreply.github.com>
2019-06-05 11:20:23 +0800

    Create 模拟实现strcpy.c

---
+++ 模拟实现strcpy.c
@@ -0,0 +1,26 @@
+//模拟实现strcpy(liangzhongfangfa )
+//strcpy把含有'\0'结束符的字符串复制到另一个地址空间，返回值的类型为char*。
+#include <assert.h>
+//char* my_strcpy(char* dest, char* src)
+char* my_strcpy(char* dest, const char* src)
+{
+	char* ret = dest;
+	assert(dest != NULL);
+	assert(src != NULL);
+	while (*dest = *src)
+	{
+		src++;
+		dest++;
+	}
+	return ret;
+
+}
+
+int main()
+{
+	char arr1[20] = { 0 };
+	char arr2[] = "hello world";
+	printf("%s\n", my_strcpy(arr1, arr2));
+	system("pause");
+	return 0;
+}
