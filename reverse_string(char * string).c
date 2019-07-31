17fc8493dc4ccac12688773e1fdde34fbd831698
kobe1916 <47083794+kobe1916@users.noreply.github.com>
2019-07-31 22:51:36 +0800

    Update reverse_string(char * string).c

--- reverse_string(char * string).c
+++ reverse_string(char * string).c
@@ -1,5 +1,5 @@
 //字符串逆序  abcdefg --> gfedcba
-
+//reverse_string(char * string)
 #include <stdio.h>
 #include <stdlib.h>
  
@@ -21,3 +21,12 @@ int main()
     system("pause");
     return 0;
 }
+//原理：递归实现
+//从首字符地址开始   若不为0地址加一，知道找到'\0'结尾 
+//此时地址减一，找到前一个字符开始打印
+//该字符打印后返回上次调用 接着自减打印下一字符依次循环
+
+
+
+
+//关于递归和迭代：https://www.cnblogs.com/zhizhan/p/4892886.html
