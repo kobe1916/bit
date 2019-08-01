a8b1265fbc540330292fa298ca8cb326789df1cd
kobe1916 <47083794+kobe1916@users.noreply.github.com>
2019-08-01 20:10:34 +0800

    Update 字符串反转.c

--- 字符串反转.c
+++ 字符串反转.c
@@ -1,1 +1,20 @@
-
+#include <stdlib.h>
+#include <stdio.h>
+void res(char *str)
+{
+  str++;
+  if(*str != '\0')
+  {
+    res(str);
+  }
+  str--;
+  printf("%c",*str);
+}
+int main()
+{
+  char *p = "adcdefgh";
+  res(p);
+  printf("\n");
+  system("pause");
+  return 0;
+}
